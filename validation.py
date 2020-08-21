9e68af05aa8756d8c8c3e17129b14aacea0b6bc5
junya <jyunya.uchida@gmail.com>
2020-08-21 11:52:37 +0900

    tiny modify

--- validation.py
+++ validation.py
@@ -139,5 +139,5 @@ def val_epoch(epoch, data_loader, model, criterion, epoch_logger, opts):
         d_2.imshow(numpy_target[3], cmap='jet')
         d_3.imshow(numpy_out[3], cmap='jet')
 
-        plt.savefig(os.path.join(opts.results_path, 'images', 'shanghaitech_partB_tr_fromMSE_{}.png'.format(epoch)))
+        plt.savefig(os.path.join(opts.results_path, 'images', 'shanghaitech_partB_vl_fromMSE_{}.png'.format(epoch)))
         plt.close(fig)
